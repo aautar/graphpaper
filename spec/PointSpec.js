b6bea76261ac83662b0e5e2a47f8d11060ed21fd
Avishkar Autar <aautar@digital-radiation.com>
2017-06-25 00:41:51 -0400

    test for Point.getY

--- spec/PointSpec.js
+++ spec/PointSpec.js
@@ -6,5 +6,10 @@ describe("Point", function() {
     var p = new Point(-100, 200);
     expect(p.getX()).toBe(-100);
   });
+
+  it("getY returns y coordinate", function() {
+    var p = new Point(-100, 200);
+    expect(p.getY()).toBe(200);
+  });  
   
 });
