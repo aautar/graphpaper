4cc8f5708e4a34bbe623292254dbf0205f692f82
Avishkar Autar <aautar@digital-radiation.com>
2017-06-25 19:15:28 -0400

    tests for Dimensions func

---
+++ spec/DimensionsSpec.js
@@ -0,0 +1,15 @@
+import {Dimensions} from '../src/Dimensions'
+
+describe("Dimensions", function() {
+ 
+  it("getWidth returns width", function() {
+    var p = new Dimensions(100, 200);
+    expect(p.getWidth()).toBe(100);
+  });
+
+  it("getY returns y coordinate", function() {
+    var p = new Dimensions(100, 200);
+    expect(p.getHeight()).toBe(200);
+  });  
+  
+});
