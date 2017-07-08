db92ac7bc0ca0d399c74d01142b22c83bf99e2cd
Avishkar Autar <aautar@digital-radiation.com>
2017-07-08 18:41:25 -0400

    helper method in test to create CanvasObject mock

--- spec/CanvasSpec.js
+++ spec/CanvasSpec.js
@@ -2,6 +2,27 @@ import {Canvas} from '../src/Canvas.js';
 import {CanvasObject} from '../src/CanvasObject.js';
 
 describe("Canvas", function() {
+
+    function makeCanvasObject(_id, _x, _y, _width, _height) {
+        var mockDomElem = {
+            addEventListener: function() { }
+        };
+
+        var o = new CanvasObject(
+            _id,
+            _x, 
+            _y, 
+            _width, 
+            _height, 
+            {}, 
+            mockDomElem, 
+            mockDomElem, 
+            mockDomElem
+        );
+
+        return o;
+    };
+
   it("snapToGrid snaps coordinate to grid", function() {
     const canvas = new Canvas({}, {});
     const snappedValue = canvas.snapToGrid(13);
@@ -10,23 +31,7 @@ describe("Canvas", function() {
 
   it("getObjectById returns added object", function() {
     const canvas = new Canvas({}, {});
-
-    var mockDomElem = {
-      addEventListener: function() { }
-    };
-
-    var o = new CanvasObject(
-        "obj-123",
-        100, 
-        200, 
-        10, 
-        20, 
-        {}, 
-        mockDomElem, 
-        mockDomElem, 
-        mockDomElem
-    );
-
+    var o = makeCanvasObject("obj-123", 100, 200, 10, 20);
     canvas.addObject(o);
 
     const retunedCanvasObject = canvas.getObjectById('obj-123');
@@ -36,22 +41,7 @@ describe("Canvas", function() {
   it("getObjectById returns null for missing object", function() {
     const canvas = new Canvas({}, {});
 
-    var mockDomElem = {
-      addEventListener: function() { }
-    };
-
-    var o = new CanvasObject(
-        "obj-123",
-        100, 
-        200, 
-        10, 
-        20, 
-        {}, 
-        mockDomElem, 
-        mockDomElem, 
-        mockDomElem
-    );
-
+    var o = makeCanvasObject("obj-123", 100, 200, 10, 20);
     canvas.addObject(o);
 
     const retunedCanvasObject = canvas.getObjectById('id-for-non-existent-object');
@@ -65,18 +55,7 @@ describe("Canvas", function() {
       addEventListener: function() { }
     };
 
-    var o = new CanvasObject(
-        "obj-123",
-        100, 
-        200, 
-        10, 
-        20, 
-        {}, 
-        mockDomElem, 
-        mockDomElem, 
-        mockDomElem
-    );
-
+    var o = makeCanvasObject("obj-123", 100, 200, 10, 20);
     canvas.addObject(o);
 
     const retunedCanvasObjects = canvas.getObjectsAroundPoint(99, 201);
@@ -89,22 +68,7 @@ describe("Canvas", function() {
   it("getObjectsAroundPoint does not return objects outside box-radius of 1px", function() {
     const canvas = new Canvas({}, {});
 
-    var mockDomElem = {
-      addEventListener: function() { }
-    };
-
-    var o = new CanvasObject(
-        "obj-123",
-        100, 
-        200, 
-        10, 
-        20, 
-        {}, 
-        mockDomElem, 
-        mockDomElem, 
-        mockDomElem
-    );
-
+    var o = makeCanvasObject("obj-123", 100, 200, 10, 20);
     canvas.addObject(o);
 
     const retunedCanvasObjects = canvas.getObjectsAroundPoint(111, 221);
