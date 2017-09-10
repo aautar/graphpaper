e4ba55ffcde17c8f1c1224bc2f44631d697f98aa
Avishkar Autar <aautar@digital-radiation.com>
2017-09-09 23:31:58 -0400

    negative test for BoxClusterDetector::areObjectsClose

--- spec/BoxClusterDetectorSpec.js
+++ spec/BoxClusterDetectorSpec.js
@@ -37,9 +37,45 @@ describe("BoxClusterDetector::areObjectsClose", function() {
         const isClose = detector.areObjectsClose(o1, o2);
         expect(isClose).toEqual(true);
       });
+
+  
+      it("returns false for 2 objects not near each other", function() {
+        
+            const mockDomElem = {
+              addEventListener: function() { }
+            };
+      
+            const o1 = new CanvasObject(
+                "obj-123",
+                100, 
+                200, 
+                10, 
+                20, 
+                {}, 
+                mockDomElem, 
+                mockDomElem, 
+                mockDomElem
+            );
+      
+            const o2 = new CanvasObject(
+              "obj-456",
+              135, 
+              222, 
+              10, 
+              20, 
+              {}, 
+              mockDomElem, 
+              mockDomElem, 
+              mockDomElem
+          );        
+        
+            const detector = new BoxClusterDetector(12.0);
+            const isClose = detector.areObjectsClose(o1, o2);
+            expect(isClose).toEqual(false);
+          });      
     
 });
-    
+
 
 describe("BoxClusterDetector::getAllObjectsCloseTo", function() {
 
