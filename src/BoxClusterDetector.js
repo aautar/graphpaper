cc70966d4d1c1c98c159105a8c187b1f180ba4f1
Avishkar Autar <aautar@digital-radiation.com>
2017-09-09 23:15:05 -0400

    work on BoxClusterDetector

--- src/BoxClusterDetector.js
+++ src/BoxClusterDetector.js
@@ -1,9 +1,74 @@
 import {Rectangle} from './Rectangle';
 import  {CanvasObject} from './CanvasObject';
+import  {Cluster} from './Cluster';
 
 function BoxClusterDetector(_boxExtentOffset) {
 
     const self = this;
+
+    /**
+     * 
+     * @param {CanvasObject} _obj 
+     * @param {CanvasObject[]} _canvasObjectsArray 
+     * @returns {Number}
+     */
+    const getObjectIndexFromArray = function(_obj, _canvasObjectsArray) {
+        for(let i=0; i<_canvasObjectsArray.length; i++) {
+            if(_canvasObjectsArray.getId() === _obj.getId()) {
+                return i;
+            }
+        }
+
+        return -1;
+    };
+
+    /**
+     * 
+     * @param {CanvasObject[]} _objects 
+     * @param {CanvasObject[]} _canvasObjectsArray 
+     * @returns {CanvasObject[]}
+     */
+    const removeObjectsFromArray = function(_objects, _canvasObjectsArray) {
+        for(let i=0; i<_objects.length; i++) {
+            const idx = getObjectIndexFromArray(_objects[i].getId(), _canvasObjectsArray);
+            _canvasObjectsArray.splice(idx, 1);
+        }
+
+        return _canvasObjectsArray;
+    };
+
+    /**
+     * 
+     * @param {Map<Cluster, Number>} _clusterToObjectCountMap 
+     * @returns {Cluster}
+     */
+    const getClusterWithMostObjectsFromClusterMap = function(_clusterToObjectCountMap)
+    {
+        var curMaxObjs = 0;
+        var curClusterWithMax = null;
+
+        _clusterToObjectCountMap.forEach(function(_numObjs, _cluster, _map) {
+            if(_numObjs > curMaxObjs) {
+                curMaxObjs = _numObjs;
+                curClusterWithMax = _cluster;
+            }
+        });
+
+        return curClusterWithMax;
+    };
+
+    /**
+     * @param {Map<Cluster, Number>} _clusterToObjectCountMap 
+     * @returns {Cluster[]}
+     */
+    const getClusterArrayFromClusterMap = function(_clusterToObjectCountMap) {
+        const resultSet = [];
+        _clusterToObjectCountMap.keys().forEach(function(_cluster) {
+            resultSet.push(_cluster);
+        });
+
+        return resultSet;
+    };
 
     /**
      * @param {CanvasObject} _objA
@@ -15,7 +80,7 @@ function BoxClusterDetector(_boxExtentOffset) {
         const nB = new Rectangle(_objB.x-_boxExtentOffset, _objB.y-_boxExtentOffset, _objB.x + _objB.width + _boxExtentOffset, _objB.y + _objB.height + _boxExtentOffset);
         return nA.checkIntersect(nB);
     };
-
+   
     /**
      * @param {CanvasObject} _obj
      * @param {CanvasObject[]} _objectsUnderConsideration
@@ -24,7 +89,7 @@ function BoxClusterDetector(_boxExtentOffset) {
     this.getAllObjectsCloseTo = function(_obj, _objectsUnderConsideration) {
         const resultSet = [];
         for(let i=0; i<_objectsUnderConsideration.length; i++) {
-            if(_objectsUnderConsideration[i] === null || _obj.getId() === _objectsUnderConsideration[i].id) {
+            if(_obj.getId() === _objectsUnderConsideration[i].id) {
                 continue;
             }
 
@@ -34,7 +99,108 @@ function BoxClusterDetector(_boxExtentOffset) {
         }
 
         return resultSet;
+    };
+
+    /**
+     * @param {CanvasObject} _seedObj
+     * @param {CanvasObject[]} _objectsUnderConsideration
+     * @param {CanvasObject[]} _resultSet
+     */
+    this.getClusterObjectsFromSeed = function(_seedObj, _objectsUnderConsideration, _resultSet) {
+        const closeByObjects = self.getAllObjectsCloseTo(_seedObj, _objectsUnderConsideration);
+        if(closeByObjects.length <= 0) {
+            return [];
+        } else {
+            removeObjectsFromArray(closeByObjects, _objectsUnderConsideration);
+
+            closeByObjects.forEach(function(_note) {
+                _resultSet.push(_note);
+                self.getClusterObjectsFromSeed(_note, _objectsUnderConsideration, _resultSet);
+            });
+        }
+    };
+
+
+    /**
+     * @param {CanvasObject[]} _objs
+     * @param {Cluster[]} _clusters
+     * @returns {Map<Cluster,Number>}
+     */
+    this.findIntersectingClustersForObjects = function(_objs, _clusters) {
+
+        // Map of Cluster that is intersecting to number of objects in _objs that is intersecting the given Cluster
+        const intersectingClusterToNumObjectsIntersecting = new Map();
+
+        _clusters.forEach(function(_cluster) {
+
+            const clusterObjs = _cluster.getObjects();
+
+            for(let i=0; i<clusterObjs.length; i++) {
+                for(let j=0; j<_objs.length; j++) {
+
+                    if(clusterObjs[i].getId() !== _objs[j].getId()) {
+                        continue;
+                    }
+
+                    if(intersectingClusterToNumObjectsIntersecting.has(_cluster)) {
+                        const count = intersectingClusterToNumObjectsIntersecting.get(_cluster);
+                        intersectingClusterToNumObjectsIntersecting.set(_cluster, count+1);
+                    } else {
+                        intersectingClusterToNumObjectsIntersecting.set(_cluster, 1);
+                    }
+                }
+            }
+        });
+
+        return intersectingClusterToNumObjectsIntersecting;
+    };
+
+    /**
+     * @param {CanvasObject} _obj
+     * @param {Cluster[]} _clusters
+     */
+    this.removeObjectFromClusters = function(_obj, _clusters) {
+        _clusters.forEach(function(_c) {
+            _c.removeObjectById(_obj.getId());
+        });
     };    
+
+    this.computeClusters = function(_objects, _knownClusters) {
+        const clusters = _knownClusters.map(function(_c) {
+            return _c;
+        });
+
+        const objectsUnderConsideration = _objects.map(function(_o) {
+            return _o;
+        });
+
+        while(objectsUnderConsideration.length > 0) {
+            const obj = objectsUnderConsideration.pop();
+
+            const objsForCluster = [obj];
+            getClusterObjectsFromSeed(obj, objectsUnderConsideration, objsForCluster);
+
+            if(objsForCluster.length > 1) {
+
+                var clusterToAddTo = null;
+                const intersectingClusterToNumObjectsIntersecting = self.findIntersectingClustersForObjects(objsForCluster);
+
+                if(intersectingClusterToNumObjectsIntersecting.size === 0) {
+                    //clusterToAddTo = new Cluster()
+                } else {
+                    clusterToAddTo = getClusterWithMostObjectsFromClusterMap(intersectingClusterToNumObjectsIntersecting);
+                }
+
+
+            } else {
+                self.removeObjectFromClusters(obj, clusters);
+            }
+
+        }
+
+        return clusters;
+    };
+    
 };
 
 export { BoxClusterDetector };
