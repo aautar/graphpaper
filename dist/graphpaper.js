c256574a7ecf6fc6a54c14e1df99d4b11080320c
Avishkar Autar <aautar@digital-radiation.com>
2017-10-04 00:47:14 -0400

    center connector endpoint within anchor element

--- dist/graphpaper.js
+++ dist/graphpaper.js
@@ -152,14 +152,14 @@ function ConnectorAnchor(_domElement, _parentObject) {
      * @returns {Number}
      */     
     this.getX = function() {
-        return _parentObject.getX() + _domElement.offsetLeft;
+        return _parentObject.getX() + _domElement.offsetLeft + (_domElement.clientWidth * 0.5);
     };
 
     /**
      * @returns {Number}
      */     
     this.getY = function() {
-        return _parentObject.getY() + _domElement.offsetTop;
+        return _parentObject.getY() + _domElement.offsetTop + (_domElement.clientHeight * 0.5);
     };
 
     /**
