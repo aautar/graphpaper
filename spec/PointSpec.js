c17c98a9e00c863236b95e090aa0c877b907c141
Avishkar Autar <aautar@digital-radiation.com>
2017-10-14 20:49:41 -0400

    Point method to get Point in cartesian coordinates

--- spec/PointSpec.js
+++ spec/PointSpec.js
@@ -1,15 +1,27 @@
 import {Point} from '../src/Point'
 
-describe("Point", function() {
+describe("Point.getX", function() {
  
-  it("getX returns x coordinate", function() {
-    var p = new Point(-100, 200);
+  it("returns x coordinate", function() {
+    const p = new Point(-100, 200);
     expect(p.getX()).toBe(-100);
   });
+});
 
-  it("getY returns y coordinate", function() {
-    var p = new Point(-100, 200);
+describe("Point.getY", function() {  
+  it("returns y coordinate", function() {
+    const p = new Point(-100, 200);
     expect(p.getY()).toBe(200);
   });  
+});
   
+describe("Point.getCartesianPoint", function() {  
+  it("returns a Point in cartesian coordinates", function() {
+    const screenPoint = new Point(25, 75);
+    const cartesianPoint = screenPoint.getCartesianPoint(100, 100);
+
+    expect(cartesianPoint.getX()).toBe(-25);
+    expect(cartesianPoint.getY()).toBe(-25);
+  });  
+
 });
--- src/Point.js
+++ src/Point.js
@@ -18,6 +18,16 @@ function Point(_x, _y) {
     this.getY = function() {
         return _y;
     };
+
+    /**
+     * @returns {Point}
+     */
+    this.getCartesianPoint = function(_canvasWidth, _canvasHeight) {
+        return new Point(
+            _x - (_canvasWidth * 0.5),
+            -_y + (_canvasHeight * 0.5)
+        );
+    };
 };
 
 export { Point };
