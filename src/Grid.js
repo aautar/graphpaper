c1b6ca1a31d3fb5a3e09fc54dbd4099e84e69a50
Avishkar Autar <aautar@digital-radiation.com>
2017-10-21 23:09:11 -0400

    jsdocs for Grid methods

--- src/Grid.js
+++ src/Grid.js
@@ -11,14 +11,23 @@ const GRID_STYLE = {
  */
 function Grid(_size, _color, _style) {
 
+    /**
+     * @returns {Number}
+     */
     this.getSize = function() {
         return _size;
     };
 
+    /**
+     * @returns {String}
+     */
     this.getStyle = function() {
         return _style;
     };
 
+    /**
+     * @returns {String}
+     */
     this.getSvgImageTile = function() {
         if(_style === GRID_STYLE.LINE) {
             return '<svg xmlns="http://www.w3.org/2000/svg" width="' + _size + '" height="' + _size + '"><rect width="12" height="1" x="0" y="11" style="fill:' + _color + '" /><rect width="1" height="12" x="11" y="0" style="fill:' + _color + '" /></svg>';
