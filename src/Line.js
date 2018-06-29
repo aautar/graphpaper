10cd27cb162965dde830043daeec25b662fa4e25
Avishkar Autar <aautar@digital-radiation.com>
2018-06-28 23:01:16 -0400

    refactor to optimize Point methods by declaring functions on prototype chain

--- src/Line.js
+++ src/Line.js
@@ -7,117 +7,118 @@ import  {LINE_INTERSECTION_TYPE, LineIntersection} from './LineIntersection';
  * @param {Point} _endPoint
  */
 function Line(_startPoint, _endPoint) {   
+    this.__startPoint = _startPoint;
+    this.__endPoint = _endPoint;
+};
 
-    const self = this;
 
-    /**
-     * @returns {Point}
-     */       
-    this.getStartPoint = function() {
-        return _startPoint;
-    };
+/**
+ * @returns {Point}
+ */       
+Line.prototype.getStartPoint = function() {
+    return this.__startPoint;
+};
 
-    /**
-     * @returns {Point}
-     */       
-    this.getEndPoint = function() {
-        return _endPoint;
-    };
+/**
+ * @returns {Point}
+ */       
+Line.prototype.getEndPoint = function() {
+    return this.__endPoint;
+};
 
-    /**
-     * @param {Line} _otherLine
-     * @returns {Boolean}
-     */
-    this.isEqual = function(_otherLine) {
-        if(self.getStartPoint().isEqual(_otherLine.getStartPoint()) && self.getEndPoint().isEqual(_otherLine.getEndPoint())) {
-            return true;
-        }
+/**
+ * @param {Line} _otherLine
+ * @returns {Boolean}
+ */
+Line.prototype.isEqual = function(_otherLine) {
+    if(this.getStartPoint().isEqual(_otherLine.getStartPoint()) && this.getEndPoint().isEqual(_otherLine.getEndPoint())) {
+        return true;
+    }
 
-        return false;
-    };
+    return false;
+};
 
-    /**
-     * @returns {Number}
-     */
-    this.getLength = function() {
-        return Math.sqrt(
-            Math.pow(_endPoint.getX() - _startPoint.getX(), 2) + Math.pow(_endPoint.getY() - _startPoint.getY(), 2)
-        );
-    };
+/**
+ * @returns {Number}
+ */
+Line.prototype.getLength = function() {
+    return Math.sqrt(
+        Math.pow(this.__endPoint.getX() - this.__startPoint.getX(), 2) + Math.pow(this.__endPoint.getY() - this.__startPoint.getY(), 2)
+    );
+};
 
-    /**
-     * @param {Line} _otherLine
-     * @returns {LINE_INTERSECTION_TYPE}
-     */
-    this.computeIntersectionType = function(_otherLine) {
-        const thisLineStartPointX = _startPoint.getX();
-        const thisLineStartPointY = _startPoint.getY();
-        const thisLineEndPointX = _endPoint.getX();
-        const thisLineEndPointY = _endPoint.getY();        
-        const otherLineStartPointX = _otherLine.getStartPoint().getX();
-        const otherLineStartPointY = _otherLine.getStartPoint().getY();
-        const otherLineEndPointX = _otherLine.getEndPoint().getX();
-        const otherLineEndPointY = _otherLine.getEndPoint().getY();
+/**
+ * @param {Line} _otherLine
+ * @returns {LINE_INTERSECTION_TYPE}
+ */
+Line.prototype.computeIntersectionType = function(_otherLine) {
+    const thisLineStartPointX = this.__startPoint.getX();
+    const thisLineStartPointY = this.__startPoint.getY();
+    const thisLineEndPointX = this.__endPoint.getX();
+    const thisLineEndPointY = this.__endPoint.getY();        
+    const otherLineStartPointX = _otherLine.getStartPoint().getX();
+    const otherLineStartPointY = _otherLine.getStartPoint().getY();
+    const otherLineEndPointX = _otherLine.getEndPoint().getX();
+    const otherLineEndPointY = _otherLine.getEndPoint().getY();
 
-        const paramDenom = (otherLineEndPointY-otherLineStartPointY)*(thisLineEndPointX-thisLineStartPointX) - (otherLineEndPointX-otherLineStartPointX)*(thisLineEndPointY-thisLineStartPointY);
-        const paramANumer = (otherLineEndPointX-otherLineStartPointX)*(thisLineStartPointY-otherLineStartPointY) - (otherLineEndPointY - otherLineStartPointY)*(thisLineStartPointX-otherLineStartPointX);
-        const paramBNumer = (thisLineEndPointX-thisLineStartPointX)*(thisLineStartPointY-otherLineStartPointY) - (thisLineEndPointY-thisLineStartPointY)*(thisLineStartPointX-otherLineStartPointX);
+    const paramDenom = (otherLineEndPointY-otherLineStartPointY)*(thisLineEndPointX-thisLineStartPointX) - (otherLineEndPointX-otherLineStartPointX)*(thisLineEndPointY-thisLineStartPointY);
+    const paramANumer = (otherLineEndPointX-otherLineStartPointX)*(thisLineStartPointY-otherLineStartPointY) - (otherLineEndPointY - otherLineStartPointY)*(thisLineStartPointX-otherLineStartPointX);
+    const paramBNumer = (thisLineEndPointX-thisLineStartPointX)*(thisLineStartPointY-otherLineStartPointY) - (thisLineEndPointY-thisLineStartPointY)*(thisLineStartPointX-otherLineStartPointX);
 
-        if(paramDenom == 0) {
-            if(paramDenom == 0 && paramANumer == 0 && paramBNumer==0)
-                return LINE_INTERSECTION_TYPE.COINCIDENT;
-            else
-                return LINE_INTERSECTION_TYPE.PARALLEL;
-        }
+    if(paramDenom == 0) {
+        if(paramDenom == 0 && paramANumer == 0 && paramBNumer==0)
+            return LINE_INTERSECTION_TYPE.COINCIDENT;
+        else
+            return LINE_INTERSECTION_TYPE.PARALLEL;
+    }
 
-        const paramA = paramANumer / paramDenom;
-        const paramB = paramBNumer / paramDenom;
-       
-        if(paramA > 1.0 || paramA < 0.0 || paramB > 1.0 || paramB < 0.0) {
-            return LINE_INTERSECTION_TYPE.LINE;
-        } else {
-            return LINE_INTERSECTION_TYPE.LINESEG;
-        }
-    };
-        
-    /**
-     * @param {Line} _otherLine
-     * @returns {LINE_INTERSECTION_RESULT}
-     */
-    this.computeIntersection = function(_otherLine) {
-
-        const thisLineStartPointX = _startPoint.getX();
-        const thisLineStartPointY = _startPoint.getY();
-        const thisLineEndPointX = _endPoint.getX();
-        const thisLineEndPointY = _endPoint.getY();        
-        const otherLineStartPointX = _otherLine.getStartPoint().getX();
-        const otherLineStartPointY = _otherLine.getStartPoint().getY();
-        const otherLineEndPointX = _otherLine.getEndPoint().getX();
-        const otherLineEndPointY = _otherLine.getEndPoint().getY();
-
-        const paramDenom = (otherLineEndPointY-otherLineStartPointY)*(thisLineEndPointX-thisLineStartPointX) - (otherLineEndPointX-otherLineStartPointX)*(thisLineEndPointY-thisLineStartPointY);
-        const paramANumer = (otherLineEndPointX-otherLineStartPointX)*(thisLineStartPointY-otherLineStartPointY) - (otherLineEndPointY - otherLineStartPointY)*(thisLineStartPointX-otherLineStartPointX);
-        const paramBNumer = (thisLineEndPointX-thisLineStartPointX)*(thisLineStartPointY-otherLineStartPointY) - (thisLineEndPointY-thisLineStartPointY)*(thisLineStartPointX-otherLineStartPointX);
+    const paramA = paramANumer / paramDenom;
+    const paramB = paramBNumer / paramDenom;
     
-        if(paramDenom == 0) {
-            if(paramDenom == 0 && paramANumer == 0 && paramBNumer==0)
-                return new LineIntersection(LINE_INTERSECTION_TYPE.COINCIDENT, null);
-            else
-                return new LineIntersection(LINE_INTERSECTION_TYPE.PARALLEL, null);
-        }
+    if(paramA > 1.0 || paramA < 0.0 || paramB > 1.0 || paramB < 0.0) {
+        return LINE_INTERSECTION_TYPE.LINE;
+    } else {
+        return LINE_INTERSECTION_TYPE.LINESEG;
+    }
+};
+    
+/**
+ * @param {Line} _otherLine
+ * @returns {LINE_INTERSECTION_RESULT}
+ */
+Line.prototype.computeIntersection = function(_otherLine) {
 
-        const paramA = paramANumer / paramDenom;
-        const paramB = paramBNumer / paramDenom;
-   
-        const xIntersect = _startPoint.getX() + paramA*(_endPoint.getX()-_startPoint.getX());
-        const yIntersect = _startPoint.getY() + paramA*(_endPoint.getY()-_startPoint.getY());
-     
-        if(paramA > 1.0 || paramA < 0.0 || paramB > 1.0 || paramB < 0.0) {
-            return new LineIntersection(LINE_INTERSECTION_TYPE.LINE, new Point(xIntersect, yIntersect));
-        } else {
-            return new LineIntersection(LINE_INTERSECTION_TYPE.LINESEG, new Point(xIntersect, yIntersect));
-        }
-    };
+    const thisLineStartPointX = this.__startPoint.getX();
+    const thisLineStartPointY = this.__startPoint.getY();
+    const thisLineEndPointX = this.__endPoint.getX();
+    const thisLineEndPointY = this.__endPoint.getY();        
+    const otherLineStartPointX = _otherLine.getStartPoint().getX();
+    const otherLineStartPointY = _otherLine.getStartPoint().getY();
+    const otherLineEndPointX = _otherLine.getEndPoint().getX();
+    const otherLineEndPointY = _otherLine.getEndPoint().getY();
+
+    const paramDenom = (otherLineEndPointY-otherLineStartPointY)*(thisLineEndPointX-thisLineStartPointX) - (otherLineEndPointX-otherLineStartPointX)*(thisLineEndPointY-thisLineStartPointY);
+    const paramANumer = (otherLineEndPointX-otherLineStartPointX)*(thisLineStartPointY-otherLineStartPointY) - (otherLineEndPointY - otherLineStartPointY)*(thisLineStartPointX-otherLineStartPointX);
+    const paramBNumer = (thisLineEndPointX-thisLineStartPointX)*(thisLineStartPointY-otherLineStartPointY) - (thisLineEndPointY-thisLineStartPointY)*(thisLineStartPointX-otherLineStartPointX);
+
+    if(paramDenom == 0) {
+        if(paramDenom == 0 && paramANumer == 0 && paramBNumer==0)
+            return new LineIntersection(LINE_INTERSECTION_TYPE.COINCIDENT, null);
+        else
+            return new LineIntersection(LINE_INTERSECTION_TYPE.PARALLEL, null);
+    }
+
+    const paramA = paramANumer / paramDenom;
+    const paramB = paramBNumer / paramDenom;
+
+    const xIntersect = this.__startPoint.getX() + paramA*(this.__endPoint.getX()-this.__startPoint.getX());
+    const yIntersect = this.__startPoint.getY() + paramA*(this.__endPoint.getY()-this.__startPoint.getY());
+    
+    if(paramA > 1.0 || paramA < 0.0 || paramB > 1.0 || paramB < 0.0) {
+        return new LineIntersection(LINE_INTERSECTION_TYPE.LINE, new Point(xIntersect, yIntersect));
+    } else {
+        return new LineIntersection(LINE_INTERSECTION_TYPE.LINESEG, new Point(xIntersect, yIntersect));
+    }
 };
 
 export { Line };
