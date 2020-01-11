8124457c2fc81e91a12c44277b7c2dc3775db452
Avishkar Autar <aautar@digital-radiation.com>
2020-01-10 21:26:15 -0500

    build fixes for ConnectorRoutingWorker.string.js, create via rollupjs plugin after creating worker dist min file

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,51 @@
 import babel from 'rollup-plugin-babel';
+import stringifyWorkersPlugin from './build/StringifyWorkersPlugin';
+
+const babelConfig = {
+  babelrc: false,
+  presets: [
+    [
+      '@babel/env', 
+      {
+        targets: {
+          "chrome": "41"
+        },       
+        "corejs": "2",
+        useBuiltIns: "usage"     
+      }
+    ],          
+    ['minify', {
+      builtIns: false,
+      deadcode: false,
+    }], 
+  ],
+  comments: false,
+  exclude: 'node_modules/**',
+};
 
 // rollup.config.js
 export default [
+  {
+    input: 'src/Workers/ConnectorRoutingWorker.js',
+    output: {
+      format: 'iife',
+      file: 'dist/workers/connector-routing-worker.js',
+      name: 'ConnectorRoutingWorker'
+    }
+  },
+  {
+    input: 'src/Workers/ConnectorRoutingWorker.js',
+    output: {
+      format: 'iife',
+      file: 'dist/workers/connector-routing-worker.min.js',
+      name: 'ConnectorRoutingWorker',
+      sourcemap: false,
+    },
+    plugins: [
+      babel(babelConfig),
+      stringifyWorkersPlugin()
+    ],        
+  },  
   {
     input: 'src/GraphPaper.js',
     output: {
@@ -19,69 +63,7 @@ export default [
       sourcemap: true  
     },
     plugins: [
-      babel({
-        babelrc: false,
-        presets: [
-          [
-            '@babel/env', 
-            {
-              targets: {
-                "chrome": "41"
-              },       
-              "corejs": "2",
-              useBuiltIns: "usage"     
-            }
-          ],          
-          ['minify', {
-            builtIns: false,
-            deadcode: false,
-          }], 
-        ],
-        comments: false,
-        
-        exclude: 'node_modules/**',
-      }),
+      babel(babelConfig),
     ],        
-  },  
-  {
-    input: 'src/Workers/ConnectorRoutingWorker.js',
-    output: {
-      format: 'iife',
-      file: 'dist/workers/connector-routing-worker.js',
-      name: 'ConnectorRoutingWorker'
-    }
-  },
-  {
-    input: 'src/Workers/ConnectorRoutingWorker.js',
-    output: {
-      format: 'iife',
-      file: 'dist/workers/connector-routing-worker.min.js',
-      name: 'ConnectorRoutingWorker',
-      sourcemap: true  
-    },
-    plugins: [
-      babel({
-        babelrc: false,
-        presets: [
-          [
-            '@babel/env', 
-            {
-              targets: {
-                "chrome": "41"
-              },       
-              "corejs": "2",
-              useBuiltIns: "usage"     
-            }
-          ],          
-          ['minify', {
-            builtIns: false,
-            deadcode: false,
-          }], 
-        ],
-        comments: false,
-        
-        exclude: 'node_modules/**',
-      }),
-    ],        
-  },    
+  }
 ];
