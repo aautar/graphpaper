a2e91cccb77cfa07b3aff4fe83438d896610da42
Avishkar Autar <aautar@digital-radiation.com>
2020-04-11 17:15:49 -0400

    tests for SvgPathBuilder.pointToLineTo

--- spec/SvgPathBuilderSpec.js
+++ spec/SvgPathBuilderSpec.js
@@ -2,9 +2,7 @@ import {SvgPathBuilder} from '../src/SvgPathBuilder'
 import { Point } from '../src/Point';
 
 describe("SvgPathBuilder.pointsToPath", function() {
-
     it("returns SVG path string with given Points", function() {
-
         const points = [];
         points.push(new Point(0, 0));
         points.push(new Point(10, 10));
@@ -16,5 +14,16 @@ describe("SvgPathBuilder.pointsToPath", function() {
 
         expect(svgPathStr).toBe("M0 0 L10 10 L20 20 L20 100 L200 500");
     });
+});
 
+describe("SvgPathBuilder.pointToLineTo", function() {
+    it("returns correct fragment for first point", function() {
+        const lineToStr = SvgPathBuilder.pointToLineTo(new Point(20, 100), 0);
+        expect(lineToStr).toBe("M20 100");
+    });
+
+    it("returns correct fragment for non-first point", function() {
+        const lineToStr = SvgPathBuilder.pointToLineTo(new Point(20, 100), 1);
+        expect(lineToStr).toBe("L20 100");
+    });
 });
