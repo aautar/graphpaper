f032076cf0e19fb5a592d76aaa9c3a197be174ef
Avishkar Autar <aautar@digital-radiation.com>
2020-04-17 18:49:39 -0400

    tests for PointVisibilityMapRouteOptimizer.optimize edge cases

--- spec/PointVisibilityMapRouteOptimizerSpec.js
+++ spec/PointVisibilityMapRouteOptimizerSpec.js
@@ -28,6 +28,58 @@ describe("PointVisibilityMapRouteOptimizer.optimize", function() {
         expect(route[2].getY()).toBe(300);                
     });
 
+    it("does no optimization for 0 points", function() {
+        const route = [
+
+        ];
+
+        const arePointsVisibleToEachOther = function(_ptA, _ptB) {
+            return true;
+        };
+
+        PointVisibilityMapRouteOptimizer.optimize(route, arePointsVisibleToEachOther);
+
+        expect(route.length).toBe(0);
+    });        
+
+    it("does no optimization for 1 point", function() {
+        const route = [
+            new Point(100, 200),
+        ];
+
+        const arePointsVisibleToEachOther = function(_ptA, _ptB) {
+            return true;
+        };
+
+        PointVisibilityMapRouteOptimizer.optimize(route, arePointsVisibleToEachOther);
+
+        expect(route.length).toBe(1);
+
+        expect(route[0].getX()).toBe(100);
+        expect(route[0].getY()).toBe(200);       
+    });    
+
+    it("does no optimization for 2 points", function() {
+        const route = [
+            new Point(0, 0),
+            new Point(100, 200),
+        ];
+
+        const arePointsVisibleToEachOther = function(_ptA, _ptB) {
+            return true;
+        };
+
+        PointVisibilityMapRouteOptimizer.optimize(route, arePointsVisibleToEachOther);
+
+        expect(route.length).toBe(2);
+
+        expect(route[0].getX()).toBe(0);
+        expect(route[0].getY()).toBe(0);
+
+        expect(route[1].getX()).toBe(100);
+        expect(route[1].getY()).toBe(200);       
+    });
+
     it("does optimization of 3 points where 1st and 3rd are visible to each other", function() {
         const route = [
             new Point(0, 0),
