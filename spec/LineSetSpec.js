e6b6fabad60e10d8e6fbba92d3ea2002d934949c
Avishkar Autar <aautar@digital-radiation.com>
2020-04-25 16:32:55 -0400

    cleanup LineSetSpec

--- spec/LineSetSpec.js
+++ spec/LineSetSpec.js
@@ -4,24 +4,24 @@ import {LineSet} from '../src/LineSet'
 
 describe("LineSet.toFloat64Array", function() {  
     it("returns array with x,y points", function() {
-      const ls = new LineSet([
-        new Line(new Point(1, 2), new Point(3, 4)),
-        new Line(new Point(5, 6), new Point(7, 8))
-      ]);
-  
-      const typedArray = ls.toFloat64Array();
-  
-      expect(typedArray.length).toBe(8);
-      expect(typedArray[0]).toBe(1);
-      expect(typedArray[1]).toBe(2);
-      expect(typedArray[2]).toBe(3);
-      expect(typedArray[3]).toBe(4);
-      expect(typedArray[4]).toBe(5);
-      expect(typedArray[5]).toBe(6);
-      expect(typedArray[6]).toBe(7);
-      expect(typedArray[7]).toBe(8);
+        const ls = new LineSet([
+            new Line(new Point(1, 2), new Point(3, 4)),
+            new Line(new Point(5, 6), new Point(7, 8))
+        ]);
+
+        const typedArray = ls.toFloat64Array();
+
+        expect(typedArray.length).toBe(8);
+        expect(typedArray[0]).toBe(1);
+        expect(typedArray[1]).toBe(2);
+        expect(typedArray[2]).toBe(3);
+        expect(typedArray[3]).toBe(4);
+        expect(typedArray[4]).toBe(5);
+        expect(typedArray[5]).toBe(6);
+        expect(typedArray[6]).toBe(7);
+        expect(typedArray[7]).toBe(8);
     });
-  });
+});
   
 describe("LineSet constructor", function() {  
     it("creates LineSet from Float64Array coordinates", function() {
