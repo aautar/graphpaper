d7ea94ed61b662ba8cdb6fe4f191247bc3a05eb0
Avishkar Autar <aautar@digital-radiation.com>
2021-02-21 15:09:50 -0500

    surface metrics for DebugMetricsPanel via depth-first scan & conversion to HTML of obj holding metrics value

--- src/DebugMetricsPanel/DebugMetricsPanel.js
+++ src/DebugMetricsPanel/DebugMetricsPanel.js
@@ -2,6 +2,28 @@ const DebugMetricsPanel = function(_window) {
     var debugPanelElem = null;
     var isVisible = false;
 
+    const objectHtmlDumpDepthFirst = function(_obj, _output, _parentName) {
+        if(typeof _parentName === 'undefined') {
+            _parentName = "";
+        }
+
+        if(_obj instanceof Object) {
+            Object.keys(_obj).forEach((_k) => {
+                if(_obj[_k] instanceof Object) {
+                    objectHtmlDumpDepthFirst(_obj[_k], _output, _k);
+                } else {
+                    let prefix = "";
+                    if(_parentName.length > 0) {
+                        prefix = _parentName + ".";
+                    }
+
+                    const metricName = `${prefix}${_k}`;
+                    _output.push(`<p class="metric">${metricName} = ${_obj[_k]}</p>`);
+                }
+            });
+        }
+    };
+
     this.init = function() {
         debugPanelElem = _window.document.createElement("div");
         debugPanelElem.classList.add("graphpaper-debug-panel");
@@ -9,11 +31,12 @@ const DebugMetricsPanel = function(_window) {
         debugPanelElem.style.position = "fixed";
         debugPanelElem.style.right = "0px";
         debugPanelElem.style.top = "0px";
-        debugPanelElem.style.width = "450px";
+        debugPanelElem.style.width = "500px";
         debugPanelElem.style.height = "200px";
         debugPanelElem.style.color = "#fff";
         debugPanelElem.style.padding = "15px";
         debugPanelElem.style.backgroundColor = "rgba(0,0,0,0.75)";
+        debugPanelElem.style.zIndex = 9999;
         _window.document.body.appendChild(debugPanelElem);
     };
 
@@ -32,16 +55,9 @@ const DebugMetricsPanel = function(_window) {
             return;
         }
 
-        debugPanelElem.innerHTML = `
-            <p>refreshAllConnectorsInternal.executionTime = ${_metrics.refreshAllConnectorsInternal.executionTime}</p>
-            <p>connectorRoutingWorker.executionTime = ${_metrics.connectorRoutingWorker.executionTime}</p>            
-            <p>-- connectorRoutingWorker.msgDecodeTime = ${_metrics.connectorRoutingWorker.msgDecodeTime}</p>
-            <p>-- connectorRoutingWorker.pointVisibilityMapCreationTime = ${_metrics.connectorRoutingWorker.pointVisibilityMapCreationTime}</p>
-            <p>-- connectorRoutingWorker.allPathsComputationTime = ${_metrics.connectorRoutingWorker.allPathsComputationTime}</p>
-            <p>-- connectorRoutingWorker.numRoutingPoints = ${_metrics.connectorRoutingWorker.numRoutingPoints}</p>
-            <p>-- connectorRoutingWorker.numBoundaryLines = ${_metrics.connectorRoutingWorker.numBoundaryLines}</p>
-            <p>connectorsRefreshTime = ${_metrics.connectorsRefreshTime}</p>
-        `;
+        const htmlStringsArr = [];
+        objectHtmlDumpDepthFirst(_metrics, htmlStringsArr);
+        debugPanelElem.innerHTML = htmlStringsArr.join('');
     };
 
 };
