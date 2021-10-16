63ce437850baec2857570697b9857a2a4eb057a2
Avishkar Autar <aautar@digital-radiation.com>
2021-10-15 23:46:46 -0400

    introduce ClusterDetectionWorker

--- src/Workers/ClusterDetectionWorker.js
+++ src/Workers/ClusterDetectionWorker.js
@@ -1,3 +1,56 @@
-onmessage = function(_req) {
+import {BoxClusterDetector} from '../BoxClusterDetector';
+import {UUID} from '../UUID';
 
+const workerData = {
+    requestQueue: [],
+    knownClusters: []
+};
+
+/**
+ * 
+ * @param {Cluster[]} _clusters 
+ * @returns {Object[]}
+ */
+const clustersToTransferrableObject = function(_clusters) {
+    const result = [];
+
+    for(let i=0; i<_clusters.length; i++) {
+        const clusterObj = _clusters[i].toJSON();
+        result.push(clusterObj);
+    }
+
+    return result;
+};
+
+const processRequestQueue = function() {
+    if(workerData.requestQueue.length === 0) {
+        return;
+    }
+
+    // grab last request, toss the rest
+    const lastRequest = workerData.requestQueue.pop();
+    workerData.requestQueue.length = 0;
+
+    // process request
+    const entityDescriptors = lastRequest.entityDescriptors;
+    const clusterDetector = new BoxClusterDetector(12.0);
+
+    const clusterDetectResult = clusterDetector.computeClusters(entityDescriptors, workerData.knownClusters, UUID.v4);
+
+    postMessage(
+        {
+            "clusters": clustersToTransferrableObject(clusterDetectResult.clusters),
+            "newClusterIds": clusterDetectResult.newClusterIds,
+            "updatedClusterIds": clusterDetectResult.updatedClusterIds,
+            "deletedClusterIds": clusterDetectResult.deletedClusterIds,
+        }
+    );
+
+    workerData.knownClusters = clusterDetectResult.clusters;
+};
+
+setInterval(processRequestQueue, 50);
+
+onmessage = function(_req) {
+    workerData.requestQueue.push(_req.data);
 };
